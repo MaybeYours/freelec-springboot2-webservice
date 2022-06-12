43996963c36914d80e19801cc171ccc3daf55b64
HaMsCodE <gkarudtlr@nate.com>
2022-06-12 22:27:29 +0900

    2022.06.12 test

--- src/main/java/com/jojodu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojodu/book/springboot/web/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController //컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 준다. 예전의 @ResponsBody 를 각 메소드마다 선언한것을 한방에!!!
 public class HelloController {
 
-    @GetMapping("/hello") //Get 의 요청을 받을 수 있는 API 생성
+    @GetMapping("/hello") //Get 의 요청을 받을 수 있는 API
     public String hello(){
         return "hello";
     }
